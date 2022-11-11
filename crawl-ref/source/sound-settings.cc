0909b0fd17798efd0e208277a075900a4bd3ff99
Loumus <113127722+Loumus@users.noreply.github.com>
2022-11-11 15:57:02 -0500

    Update sound-settings.cc

--- crawl-ref/source/sound-settings.cc
+++ crawl-ref/source/sound-settings.cc
@@ -23,8 +23,10 @@ int main()
 
             ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR))); 
 
-          
             GuiGroupBox((Rectangle){ 528, 152, 192, 96 }, "Sound Settings");
+       
+                   // TODO: Attach function to slider float value once function is written into header file
+       
             Slider001Value = GuiSlider((Rectangle){ 576, 192, 112, 16 }, "Volume", NULL, Slider001Value, 0, 100);
             
 
