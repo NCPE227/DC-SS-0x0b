84caa40dbf41282f1a226cc488c33ee0e748af07
PlanetMars1 <94670473+PlanetMars1@users.noreply.github.com>
2022-11-11 20:20:34 -0500

    Create IOFILE.cpp

---
+++ crawl-ref/source/IOFILE.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+
+
+void SaveFile(){
+
+    ifstream FileName("nameofFile")
+
+    if(!FileName){
+        return -1;
+
+    }else{
+
+        // game is saved
+    }
+}
+
+
+
+
+void LoadFile(){
+
+    ofstream Loadingfile("nameofFile")
+
+    if(!Loadingfile){
+        return -1;
+
+    }else{
+
+        // game is loaded
+    }
+
+    
+
+
+
+
+}
